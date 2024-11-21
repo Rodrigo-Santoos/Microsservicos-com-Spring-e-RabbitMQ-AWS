f4892563e00cbf5f5743086807da281feae1a133
Rodrigo-Santoos <rodrigoyukimide@gmail.com>
2024-11-21 16:42:26 -0300

    first commit

---
+++ oliveira-app/src/main/java/com/oliveira/oliveira_app/OliveiraAppApplication.java
@@ -0,0 +1,13 @@
+package com.oliveira.oliveira_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveiraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveiraAppApplication.class, args);
+	}
+
+}
---
+++ oliveira-app/src/test/java/com/oliveira/oliveira_app/OliveiraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.oliveira_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveiraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
