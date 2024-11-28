402b8c047d92509057c9d9531da4422f7632ce77
Rodrigo-Santoos <rodrigoyukimide@gmail.com>
2024-11-28 14:04:30 -0300

    adicinando o mapStruct no POM

--- proposta-app/src/main/java/com/oliveira/oliveira_app/entity/Proposta.java
+++ proposta-app/src/main/java/com/oliveira/oliveira_app/entity/Proposta.java
@@ -1,5 +1,6 @@
 package com.oliveira.oliveira_app.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -34,7 +35,7 @@ public class Proposta {
 	
 	private String observacao;
 	
-	@OneToOne
+	@OneToOne(cascade = CascadeType.PERSIST)
 	@JoinColumn(name = "id_usuario")
 	private Usuario usuario;
 
